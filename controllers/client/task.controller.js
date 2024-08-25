15aca95da53dbd056d989dc99055346c3007a92c
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-25 17:50:57 +0700

    6. Cấu hình project theo mô hình MVC

---
+++ controllers/client/task.controller.js
@@ -0,0 +1,28 @@
+const Task = require("../../models/task.model");
+
+// [GET] /tasks
+module.exports.index = async (req, res) => {
+    const tasks = await Task.find({
+        deleted: false
+    });
+    
+    res.json(tasks);
+}
+
+// [GET] /tasks/detail/:id
+module.exports.detail = async (req, res) => {
+    try {
+        const id = req.params.id;
+
+        const task = await Task.findOne({
+            _id: id,
+            deleted: false
+        });
+
+        res.json(task);
+    } catch (error) {
+        res.json({
+            message: "Not Found"
+        });
+    }
+}
