f76b82ff0f34c91a321f2484d299e7ba68db9375
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-25 17:56:51 +0700

    8. Bộ lọc theo trạng thái

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -2,9 +2,19 @@ const Task = require("../../models/task.model");
 
 // [GET] /tasks
 module.exports.index = async (req, res) => {
-    const tasks = await Task.find({
-        deleted: false
-    });
+    const find = {
+        deleted: false 
+    };
+
+    // Lọc theo trạng thái
+    const status = req.query.status;
+    
+    if(status){
+        find.status = status;
+    }
+    // Hết Lọc theo trạng thái
+
+    const tasks = await Task.find(find);
     
     res.json(tasks);
 }
