0e0756e5dd1106ac8af943b10aaa52fc500288de
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-27 17:23:58 +0700

    4. Đổi trạng thái nhiều công việc

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -74,14 +74,14 @@ module.exports.detail = async (req, res) => {
     }
 }
 
-// [PATCH] /tasks/chang-status/:id
+// [PATCH] /tasks/chang-status
 module.exports.changeStatus = async (req, res) => {
     try {
-        const id = req.params.id;
+        const ids = req.body.ids;
         const status = req.body.status;
 
-        const task = await Task.updateOne({
-            _id: id
+        const task = await Task.updateMany({
+            _id: { $in: ids }
         }, {
             status: status
         });
