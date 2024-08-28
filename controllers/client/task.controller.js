0680ad0cc7f73bd37f7230a6ae25d57425bf6319
tranduong18 <147843344+tranduong18@users.noreply.github.com>
2024-08-28 10:24:34 +0700

    7. Lấy ra danh sách task theo đúng user

--- controllers/client/task.controller.js
+++ controllers/client/task.controller.js
@@ -3,6 +3,10 @@ const Task = require("../../models/task.model");
 // [GET] /tasks
 module.exports.index = async (req, res) => {
     const find = {
+        $or: [
+            { createdBy: req.user.id },
+            { listUser: req.user.id }
+        ],
         deleted: false 
     };
 
